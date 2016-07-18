8c3f6755364592c117dffece0e0b105e9195e4b9
Justin Reid <jlreid@alumni.unca.edu>
2016-07-18 19:58:59 -0400

    Added additional package entry for heroku

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public/bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);

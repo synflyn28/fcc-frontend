8971c1a1efc516944daa64d0e28fe2e2b5ab38fb
Justin Reid <jlreid@alumni.unca.edu>
2016-07-18 19:42:07 -0400

    Changed public folder access permissions.

--- public/javascripts/Portfolio/leafletMap.js
+++ public/javascripts/Portfolio/leafletMap.js
@@ -1,7 +1,7 @@
 //Contains code for basic leaflet embed
 //Justin Lynn Reid - 9/9/2015
 $(document).ready(function(){
-  var map = L.map('map').setView([35.524292, -81.142382], 15);
+  var map = L.map('map').setView([35.508550, -82.519404], 15);
   var popupHtml = '<p style="color: #333333">1106 Turtle Creek Drive Asheville, NC 28803</p>';
   popupHtml += '<p style="color: #333333"> Phone: (405)-779-6131</p>';
   popupHtml += '<p style="color: #333333">Email: <a href="mailto:jlreid@alumni.unca.edu">';
